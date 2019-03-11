e37b433b09e76b7507d8ce9e788c79614cccc785
Jarret Rose <jarretrose@gmail.com>
2019-03-11 14:41:03 -0400

    fixed null result

--- src/SeeWorkSpace.js
+++ src/SeeWorkSpace.js
@@ -1,33 +1,20 @@
 import React, { Component, Fragment } from 'react'
-import { withStyles, FormLabel, Typography } from '@material-ui/core'
+import { withStyles, Typography } from '@material-ui/core'
 import styles from './styles'
 import Card from '@material-ui/core/Card';
-import CardActions from '@material-ui/core/CardActions';
-import CardContent from '@material-ui/core/CardContent';
 import PropTypes from 'prop-types'
 import Grid from '@material-ui/core/Grid';
 import Button from '@material-ui/core/Button'
 
 /*
-ADDITION & MULTIPLICATION
-factCategory = 0 through 9
+Division
+factCategory = 1 through 9
 
 number2 increments
 number2 max is 9
-number2 min is 0
+number2 min is 1
 */
 
-
-/*
-SUBTRACTION
-factCategory = 0 through 9
-
-number2 increments
-number2 max is 9 + fact
-number2 min is the fact
-
-
-*/
 
 class SeeWorkSpace extends Component {
   constructor() {
@@ -41,28 +28,19 @@ class SeeWorkSpace extends Component {
   componentDidUpdate(prevProps) {
     console.log('COMPONENT DID UPDATE')
     const { category } = this.props
-    const { number2, factCategory} = this.state
 
     if (prevProps.category !== category) {
-
       switch (category) {
         case 'Addition':
-          this.setState({ factCategory: 0, number2: 0 })
-          break
-
+          return this.setState({ factCategory: 0, number2: 0 })
         case 'Subtraction':
-          this.setState({ factCategory: 0, number2: 0 })
-          break
-
+          return this.setState({ factCategory: 0, number2: 0 })
         case 'Multiplication':
-          this.setState({ factCategory: 0, number2: 0 })
-          break
-
+          return this.setState({ factCategory: 0, number2: 0 })
         case 'Division':
-          this.setState({ factCategory: 0, number2: 0 })
-          break
-
-        default: return null
+          return this.setState({ factCategory: 0, number2: 0 })
+        default: 
+          return null
       }
     }
   }
@@ -71,6 +49,7 @@ class SeeWorkSpace extends Component {
     const { factCategory } = this.state
     const { category } = this.props
     const targetNumber = factCategory + change
+
     if (targetNumber > 10 || targetNumber < 0) return null
     if (category === 'Subtraction') this.setState({ factCategory: targetNumber, number2: targetNumber})
     else this.setState({ factCategory: targetNumber, number2: 0 })
@@ -84,19 +63,19 @@ class SeeWorkSpace extends Component {
       case 'Addition':
       case 'Multiplication':
         if (number2 + change > 9 || number2 + change < 0) return null
-        else this.setState({ number2: number2 + change })
-        break;
+        else return this.setState({ number2: number2 + change })
       case 'Subtraction':
         if (number2 + change > factCategory + 9 || number2 + change < factCategory) return null
-        this.setState({ number2: number2 + change})
-        break;
-      case 'Division': return null
-      default: return null
+        else return this.setState({ number2: number2 + change})
+      case 'Division': 
+        return null
+      default: 
+        return null
     }
   }
 
   render() {
-    const { classes, category, symbol } = this.props
+    const { classes } = this.props
     const { number2, factCategory } = this.state
 
     let c = (num1, num2) => {
@@ -104,18 +83,14 @@ class SeeWorkSpace extends Component {
       switch(category){
         case 'Addition': 
           return num1 + num2
-          break
         case 'Multiplication': 
           return num1 * num2
-          break
         case 'Subtraction':
           return num2 - num1
-          break
         case 'Division':
           if (num2 === 0) return '--'
           else return num2 / num1
-          break
-        default: return null
+        default: return '?'
       }
     }
 
@@ -138,7 +113,7 @@ class SeeWorkSpace extends Component {
           <i className="material-icons">navigate_before</i>Previous</Button>
 
         <Button className={classes.title} onClick={() => this.handleFactCategory(1)}>
-          Next <i className="material-icons">navigate_next</i>
+          Next<i className="material-icons">navigate_next</i>
         </Button>
 
 
@@ -156,7 +131,7 @@ class SeeWorkSpace extends Component {
 
         <Button className={classes.title} onClick={() => this.handleProblemChange(1)}>
           Next <i className="material-icons">navigate_next</i></Button>
-          
+
       </Fragment>
     )
   }
